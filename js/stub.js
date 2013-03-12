134f42c59873d2d179a80020e04bda7555958354
Ben Papillon <bpapillon@peachtreebilling.com>
2013-03-12 12:10:42 -0500

    stub shows

---
+++ js/stub.js
@@ -0,0 +1,47 @@
+function stub_shows() {
+	window.showlist = [
+		{
+			artist: "Daft Punk",
+			shows: [
+				{
+					name: 'Daft Punk is Playing at My House',
+					location: 'My house',
+					day: "every day",
+					time: "all day",
+					notes: "free, all ages"
+				}
+			]
+		},
+		{
+			artist: "Mac DeMarco",
+			shows: [
+				{
+					name: 'Panache Showcase',
+					location: 'Hotel Vegas Patio',
+					day: "Friday",
+					time: "10:10PM -10:40PM",
+					notes: ""
+				},
+				{
+					name: '',
+					location: 'The Parish',
+					day: "Saturday",
+					time: "10:10PM -10:40PM",
+					notes: "21+"
+				}
+			]
+		},
+		{
+			artist: "Widowspeak",
+			shows: [
+				{
+					name: 'Terrorbird Seventh Annual SXSW 2013 Day Party',
+					location: 'Red 7',
+					day: "Tuesday",
+					time: "5:30pm",
+					notes: "free"
+				}
+			]
+		}
+	]
+}
